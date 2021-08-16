4eb8f011e5a76697bf59206eb425cead59a13ec9
cjiankai <cjiankai@sina.com>
2021-08-16 14:27:56 +0800

    added: init project

---
+++ src/main/java/com/jw/springbootaction/SpringBootActionApplication.java
@@ -0,0 +1,13 @@
+package com.jw.springbootaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActionApplication.class, args);
+    }
+
+}
